8ee8f1d4934e1d76afe527bbc9f34b569816452b
Bison <435844159@qq.com>
2015-08-31 13:14:39 +0800

    Initial Commit

---
+++ LBPersonalPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBPersonalPage
+//
+//  Created by chenlei_mac on 15/8/31.
+//  Copyright (c) 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBPersonalPage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LBPersonalPage
+//
+//  Created by chenlei_mac on 15/8/31.
+//  Copyright (c) 2015年 Bison. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
